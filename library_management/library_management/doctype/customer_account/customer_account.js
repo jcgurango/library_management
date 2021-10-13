f9859499eb12414889277fbdadfcd60290c320dd
JC Gurango <jc@jcgurango.com>
2021-10-13 14:52:47 +0800

    Implement doctype

---
+++ library_management/library_management/doctype/customer_account/customer_account.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, JC and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Customer Account', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ library_management/library_management/doctype/customer_account/test_customer_account.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, JC and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestCustomerAccount(unittest.TestCase):
+	pass
